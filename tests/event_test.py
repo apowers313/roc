af921f5b40cfae05a60a5311ee2759fe6bf552e5
Adam Powers <apowers@ato.ms>
2023-07-07 07:23:05 -0700

    fix tests

--- tests/event_test.py
+++ tests/event_test.py
@@ -1,5 +1,16 @@
-from roc.eventbus import EventBus
+from roc.component import Component
+from roc.event import EventBus
 
 
-def test_get_bus_nonexistant():
-    assert EventBus.get("foo") == None
+class TestData:
+    def __init__(self, foo: str, baz: int):
+        self.foo = foo
+        self.baz = baz
+
+
+def test_eventbus_send():
+    eb = EventBus[TestData]()
+    c = Component("test_component", "test_type")
+    eb_conn = eb.connect(c)
+    d = TestData("bar", 42)
+    eb_conn.send(d)
