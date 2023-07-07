f815485d3bd00a3b3842006b381cdfe0df36cff7
Adam Powers <apowers@ato.ms>
2023-07-06 17:16:02 -0700

    add default class

--- roc/config.py
+++ roc/config.py
@@ -1,9 +1,21 @@
+from typing import Any
+
 from dynaconf import Dynaconf, Validator
+
+
+class DefaultSetting(Validator):
+    def __init__(self, name: str, val: Any, *, must_exist: bool = True):
+        super().__init__(name, default=val, apply_default_on_none=True, must_exist=must_exist)
+
 
 settings = Dynaconf(
     envvar_prefix="ROC",
     settings_files=["settings.toml", ".secrets.toml"],
-    validators=[Validator("db_host", must_exist=True), Validator("db_port", default=7687, apply_default_on_none=True)],
+    validators=[
+        DefaultSetting("db_host", "127.0.0.1"),
+        DefaultSetting("db_port", 7687),
+        DefaultSetting("log_level", "trace"),
+    ],
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
