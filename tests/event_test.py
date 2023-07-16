f1a5aad3ff1d88467b33f5d8c956d2ddc056d5ba
Adam Powers <apowers@ato.ms>
2023-07-16 09:44:35 -0700

    test multiple event listeners

--- tests/event_test.py
+++ tests/event_test.py
@@ -13,7 +13,7 @@ class FakeData:
 
 
 class TestEventBus:
-    def test_eventbus_send(self, mocker):
+    def test_eventbus_send(self, eb_reset, mocker):
         eb = EventBus[FakeData]("test")
         stub: MagicMock = mocker.stub(name="event_callback")
         eb.subject.subscribe(stub)
@@ -30,7 +30,33 @@ class TestEventBus:
         assert e.data.foo == "bar"
         assert e.data.baz == 42
 
-    def test_eventbus_duplicate_name(self):
+    def test_eventbus_multiple_listeners(self, eb_reset, mocker):
+        eb = EventBus[FakeData]("test")
+        l1: MagicMock = mocker.stub(name="listener1")
+        l2: MagicMock = mocker.stub(name="listener2")
+        l3: MagicMock = mocker.stub(name="listener3")
+        eb.subject.subscribe(l1)
+        eb.subject.subscribe(l2)
+        eb.subject.subscribe(l3)
+
+        c = Component("test_component", "test_type")
+        eb_conn = eb.connect(c)
+        d = FakeData("bar", 42)
+        eb_conn.send(d)
+
+        assert l1.call_count == 1
+        e1 = l1.call_args.args[0]
+        assert id(e1.data) == id(d)
+
+        assert l2.call_count == 1
+        e2 = l2.call_args.args[0]
+        assert id(e2.data) == id(d)
+
+        assert l3.call_count == 1
+        e3 = l3.call_args.args[0]
+        assert id(e3.data) == id(d)
+
+    def test_eventbus_duplicate_name(self, eb_reset):
         EventBus.clear_names()
         eb1 = EventBus[FakeData]("test")
         with pytest.raises(Exception, match="Duplicate EventBus name: test"):
