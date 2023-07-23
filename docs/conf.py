7a009da47c37b83b49ecabf7a007cf7105b01b00
Adam Powers <apowers@ato.ms>
2023-07-22 22:38:39 -0700

    update sphynx options

--- docs/conf.py
+++ docs/conf.py
@@ -14,17 +14,18 @@ author = "Adam Powers"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    "sphinx.ext.autosummary",
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
     "sphinx.ext.napoleon",
-    "sphinx.ext.autosummary",
     "sphinx.ext.githubpages",
+    "sphinx.ext.viewcode",
 ]
 
 coverage_show_missing_items = True
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
+autosummary_generate = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -32,8 +33,18 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "bizstyle"
 html_static_path = ["_static"]
 
-# # find code for autodoc
-# import os
-# import sys
-
-# sys.path.insert(0, os.path.abspath(".."))
+# Napoleon settings
+napoleon_google_docstring = True
+# napoleon_numpy_docstring = True
+# napoleon_include_init_with_doc = False
+# napoleon_include_private_with_doc = False
+# napoleon_include_special_with_doc = True
+# napoleon_use_admonition_for_examples = False
+# napoleon_use_admonition_for_notes = False
+# napoleon_use_admonition_for_references = False
+# napoleon_use_ivar = False
+# napoleon_use_param = True
+# napoleon_use_rtype = True
+# napoleon_preprocess_types = False
+# napoleon_type_aliases = None
+# napoleon_attr_annotations = True
