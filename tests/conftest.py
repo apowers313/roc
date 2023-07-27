5372cb2bf25ba61de9315b4d100ba0a00d2e1adb
Adam Powers <apowers@ato.ms>
2023-07-27 07:26:23 -0700

    move towards live db for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,22 @@
-from collections.abc import Iterator
-from typing import Any
-from unittest import mock
-
 import pytest
-from helpers.db_record import clear_current_test_record, do_recording, get_query_record
+from helpers.db_record import do_recording
 
 from roc.event import EventBus
 from roc.graphdb import Edge, GraphDB, Node
 
-LIVE_DB = False
+LIVE_DB = True
 RECORD_DB = False
 
 if RECORD_DB:
     do_recording()
 
 
-def mock_raw_fetch(db: Any, query: str, *, params: dict[str, Any] | None = None) -> Iterator[Any]:
-    return get_query_record(query)
+# def mock_raw_fetch(db: Any, query: str, *, params: dict[str, Any] | None = None) -> Iterator[Any]:
+#     return get_query_record(query)
 
 
-def mock_raw_execute(db: Any, query: str, *, params: dict[str, Any] | None = None) -> None:
-    pass
+# def mock_raw_execute(db: Any, query: str, *, params: dict[str, Any] | None = None) -> None:
+#     pass
 
 
 @pytest.fixture
@@ -34,16 +30,17 @@ def clear_cache(scope="function", autouse=True):
 
 @pytest.fixture
 def mock_db(clear_cache):
-    if not LIVE_DB:
-        db = GraphDB()
-        db.db = None
-        with mock.patch.object(GraphDB, "raw_fetch", new=mock_raw_fetch):
-            with mock.patch.object(GraphDB, "raw_execute", new=mock_raw_execute):
-                yield
-    else:
-        if RECORD_DB:
-            clear_current_test_record()
-        yield
+    pass
+    # if not LIVE_DB:
+    #     db = GraphDB()
+    #     db.db = None
+    #     with mock.patch.object(GraphDB, "raw_fetch", new=mock_raw_fetch):
+    #         with mock.patch.object(GraphDB, "raw_execute", new=mock_raw_execute):
+    #             yield
+    # else:
+    #     if RECORD_DB:
+    #         clear_current_test_record()
+    #     yield
 
 
 @pytest.fixture
@@ -60,3 +57,41 @@ def clear_db():
         db.raw_execute("MATCH (n:TestNode) DETACH DELETE n")
         # delete all nodes without relationships
         db.raw_execute("MATCH (n) WHERE degree(n) = 0 DELETE n")
+
+
+# node_del_mock: Any = None
+# edge_del_mock: Any = None
+
+
+# @pytest.fixture(scope="session", autouse=True)
+# def no_del():
+#     global node_del_mock
+#     global edge_del_mock
+#     node_del_mock = mock.patch.object(Node, "__del__")
+#     edge_del_mock = mock.patch.object(Edge, "__del__")
+#     node_del_mock.start()
+#     edge_del_mock.start()
+#     yield
+#     node_del_mock.stop()
+#     edge_del_mock.stop()
+
+
+# @pytest.fixture
+# def allow_del():
+#     if node_del_mock:
+#         print("--- STOPPING NODE DEL MOCK")
+#         node_del_mock.stop()
+
+#     if edge_del_mock:
+#         print("--- STOPPING EDGE DEL MOCK")
+#         edge_del_mock.stop()
+
+#     yield
+
+#     if node_del_mock:
+#         print("+++ STARTING NODE DEL MOCK")
+#         node_del_mock.start()
+
+#     if edge_del_mock:
+#         print("+++ STARTING EDGE DEL MOCK")
+#         edge_del_mock.start()
