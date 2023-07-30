4d4538bcc0ce16a04feeea9dee7a5c186ffd6c24
Adam Powers <apowers@ato.ms>
2023-07-30 07:16:31 -0700

    add node delete, edge delete, simplify tests

--- tests/helpers/util.py
+++ tests/helpers/util.py
@@ -4,3 +4,11 @@ import re
 def normalize_whitespace(s: str) -> str:
     s = s.strip().replace("\n", " ")
     return re.sub(r"\s+", " ", s)
+
+
+def assert_similar(expected: str, actual: str, changes: list[tuple[str, str]]) -> None:
+    match_str = re.escape(expected)
+    for change in changes:
+        print(f"replacing '{change[0]}' with '{change[1]}'")
+        match_str = match_str.replace(change[0], change[1])
+    assert re.search(match_str, actual), f"expected '{expected}' to be similar to '{actual}'"
