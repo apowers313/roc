b4ecb38fe8f53248bc103819ca3af8e51b20180f
Adam Powers <apowers@ato.ms>
2023-08-11 17:48:44 -0700

    delint

--- tests/helpers/util.py
+++ tests/helpers/util.py
@@ -9,6 +9,6 @@ def normalize_whitespace(s: str) -> str:
 def assert_similar(expected: str, actual: str, changes: list[tuple[str, str]]) -> None:
     match_str = re.escape(expected)
     for change in changes:
-        print(f"replacing '{change[0]}' with '{change[1]}'")
+        # print(f"replacing '{change[0]}' with '{change[1]}'")
         match_str = match_str.replace(change[0], change[1])
     assert re.search(match_str, actual), f"expected '{expected}' to be similar to '{actual}'"
