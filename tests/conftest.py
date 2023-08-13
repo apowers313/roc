35523f03e11913d6d6f02711cd16fce7764d7e05
Adam Powers <apowers@ato.ms>
2023-08-12 23:55:25 -0700

    add pytest emoji

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-from typing import Generator
+from typing import Any, Generator
 
 import pytest
 
@@ -43,3 +43,46 @@ def clear_db() -> Generator[None, None, None]:
     db.raw_execute("MATCH (n:TestNode) DETACH DELETE n")
     # delete all nodes without relationships
     db.raw_execute("MATCH (n) WHERE degree(n) = 0 DELETE n")
+
+
+def pytest_emoji_passed(config: Any) -> tuple[str, str]:
+    return "✅ ", "PASSED ✅ "
+
+
+def pytest_emoji_failed(config: Any) -> tuple[str, str]:
+    return "🔥 ", "FAILED 🔥"
+
+
+def pytest_emoji_skipped(config: Any) -> tuple[str, str]:
+    return "☁️ ", "SKIPPED ☁️ "
+
+
+def pytest_emoji_error(config: Any) -> tuple[str, str]:
+    return "💩 ", "ERROR 💩 "
+
+
+def pytest_emoji_xfailed(config: Any) -> tuple[str, str]:
+    return "⁉️ ", "XFAIL ⁉️ "
+
+
+def pytest_emoji_xpassed(config: Any) -> tuple[str, str]:
+    return "☘️ ", "XPASS ☘️ "
+
+
+# def pytest_emoji_passed(config: Any) -> tuple[str, str]:
+#     return "\U00002705 ", "PASSED \U00002705 "
+
+# def pytest_emoji_failed(config: Any) -> tuple[str, str]:
+#     return "\U0001F525 ", "FAILED \U0001F525"
+
+# def pytest_emoji_skipped(config: Any) -> tuple[str, str]:
+#     return "\U00002601 ", "SKIPPED \U00002601 "
+
+# def pytest_emoji_error(config: Any) -> tuple[str, str]:
+#     return "\U0001F4A9 ", "ERROR \U0001F4A9 "
+
+# def pytest_emoji_xfailed(config: Any) -> tuple[str, str]:
+#     return "⁉\U00002049 ", "XFAIL \U00002049 "
+
+# def pytest_emoji_xpassed(config: Any) -> tuple[str, str]:
+#     return "\U00002618 ", "XPASS \U00002618 "
