35a68f91b67e4ca925c7f69d48de5257c5b2ef05
Adam Powers <apowers@ato.ms>
2023-08-27 14:17:12 -0700

    delint

--- roc/__init__.py
+++ roc/__init__.py
@@ -20,27 +20,4 @@ __all__ = [
 ]
 
 
-def foo() -> None:
-    pass
 
-
-# import roc.event
-# import roc.graphdb
-# import roc.perception
-
-# from importlib import metadata as importlib_metadata
-
-# def get_version() -> str:
-#     """Gets the version of this package
-
-#     Returns:
-#         str: The version string of the package in MAJOR.MINOR.REVISION format, or unknown if the \
-#             version wasn't set.
-#     """
-#     try:
-#         return importlib_metadata.version(__name__)
-#     except importlib_metadata.PackageNotFoundError:  # pragma: no cover
-#         return "unknown"
-
-
-# version: str = get_version()
