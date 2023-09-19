4d83fb1b547717673da6584e067be9a58f90c099
Adam Powers <apowers@ato.ms>
2023-09-19 07:19:33 -0700

    add FeatureExtractor

--- tests/perception_test.py
+++ tests/perception_test.py
@@ -6,7 +6,7 @@ from helpers.nethack_screens import screens
 from helpers.util import component_response_args
 
 from roc.event import Event
-from roc.perception import DeltaData, VisionData
+from roc.perception import NONE_FEATURE, DeltaFeature, VisionData
 
 screen0 = VisionData(screen=screens[0]["chars"])
 screen1 = VisionData(screen=screens[1]["chars"])
@@ -16,12 +16,20 @@ class TestDelta:
     @component_response_args("delta", "perception", "pb_conn", [screen0, screen1])
     def test_basic(self, empty_components, component_response) -> None:
         assert isinstance(component_response, MagicMock)
-        assert component_response.call_count == 1
-        assert len(component_response.call_args.args) == 1
-        e = component_response.call_args.args[0]
+        assert component_response.call_count == 2
+        assert len(component_response.call_args_list[0].args) == 1
+
+        # first event
+        e = component_response.call_args_list[0].args[0]
         assert isinstance(e, Event)
-        assert isinstance(e.data, DeltaData)
-        diff_list = e.data.diff_list
+        assert e.data.feature is NONE_FEATURE
+
+        # second event
+        assert len(component_response.call_args_list) == 2
+        e = component_response.call_args_list[1].args[0]
+        assert isinstance(e, Event)
+        assert isinstance(e.data.feature, DeltaFeature)
+        diff_list = e.data.feature.diff_list
         assert len(diff_list) == 2
         # Diff(x=6, y=16, val1=102, val2=46), Diff(x=6, y=17, val1=46, val2=102)])
         assert diff_list[0].x == 6
