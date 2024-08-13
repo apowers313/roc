1aacc76e09b8338d7fab45cff267e69280d270e3
Adam Powers <apowers@ato.ms>
2024-08-12 19:44:20 -0700

    fix single feature when at edge of matrix

--- roc/feature_extractors/single.py
+++ roc/feature_extractors/single.py
@@ -1,3 +1,5 @@
+"""Generates Features for things that aren't like their neighbors"""
+
 from ..component import Component, register_component
 from ..location import Point
 from ..perception import Feature, FeatureExtractor, PerceptionEvent, VisionData
@@ -20,6 +22,15 @@ class Single(FeatureExtractor[Point]):
     """A component for identifying single, isolated visual features"""
 
     def event_filter(self, e: PerceptionEvent) -> bool:
+        """Filters out non-VisionData
+
+        Args:
+            e (PerceptionEvent): Any event on the perception bus
+
+        Returns:
+            bool: Returns True if the event is VisionData to keep processing it,
+            False otherwise.
+        """
         return isinstance(e.data, VisionData)
 
     def get_feature(self, e: PerceptionEvent) -> Feature | None:
@@ -28,31 +39,52 @@ class Single(FeatureExtractor[Point]):
 
         ## iterate points
         for point in data.points():
-            if (
-                # up left
-                data.get_val(point.x - 1, point.y - 1) != point.val
-                and
-                # up
-                data.get_val(point.x, point.y - 1) != point.val
-                and
-                # up right
-                data.get_val(point.x + 1, point.y - 1) != point.val
-                and
-                # left
-                data.get_val(point.x - 1, point.y) != point.val
-                and
-                # right
-                data.get_val(point.x + 1, point.y) != point.val
-                and
-                # down left
-                data.get_val(point.x - 1, point.y + 1) != point.val
-                and
-                # down
-                data.get_val(point.x, point.y + 1) != point.val
-                and
-                # down right
-                data.get_val(point.x + 1, point.y + 1) != point.val
-            ):
+            if is_unique_from_neighbors(data, point):
                 self.pb_conn.send(SingleFeature(self, point))
         self.settled()
         return None
+
+
+def is_unique_from_neighbors(data: VisionData, point: Point) -> bool:
+    """Helper function to determine if a point in a matrix has the same value as
+    any points around it.
+
+    Args:
+        data (VisionData): The matrix / Grid to evaluate
+        point (Point): The point to see if any of its neighbors have the same value
+
+    Returns:
+        bool: Returns True if the point is different from all surrounding
+        points, False otherwise.
+    """
+    max_width = data.width - 1
+    max_height = data.height - 1
+    # up left
+    if point.x > 0 and point.y > 0 and data.get_val(point.x - 1, point.y - 1) == point.val:
+        return False
+    # up
+    if point.y > 0 and data.get_val(point.x, point.y - 1) == point.val:
+        return False
+    # up right
+    if point.x < max_width and point.y > 0 and data.get_val(point.x + 1, point.y - 1) == point.val:
+        return False
+    # left
+    if point.x > 0 and data.get_val(point.x - 1, point.y) == point.val:
+        return False
+    # right
+    if point.x < max_width and data.get_val(point.x + 1, point.y) == point.val:
+        return False
+    # down left
+    if point.x > 0 and point.y < max_height and data.get_val(point.x - 1, point.y + 1) == point.val:
+        return False
+    # down
+    if point.y < max_height and data.get_val(point.x, point.y + 1) == point.val:
+        return False
+    # down right
+    if (
+        point.x < max_width
+        and point.y < max_height
+        and data.get_val(point.x + 1, point.y + 1) == point.val
+    ):
+        return False
+    return True
--- tests/single_test.py
+++ tests/single_test.py
@@ -5,7 +5,8 @@ from helpers.util import StubComponent, check_num_src_edges, check_points, check
 
 from roc.component import Component
 from roc.event import Event
-from roc.feature_extractors.single import Single, SingleFeature
+from roc.feature_extractors.single import Single, SingleFeature, is_unique_from_neighbors
+from roc.location import Point
 from roc.perception import Settled, VisionData
 
 
@@ -45,6 +46,49 @@ class TestSingle:
 
         # event 2
         e = s.output.call_args_list[1].args[0]
+        assert isinstance(e, Event)
+        assert isinstance(e.data, Settled)
+
+    def test_two_singles(self, empty_components) -> None:
+        c = Component.get("single", "perception")
+        assert isinstance(c, Single)
+        s = StubComponent(
+            input_bus=c.pb_conn.attached_bus,
+            output_bus=c.pb_conn.attached_bus,
+        )
+
+        s.input_conn.send(
+            VisionData(
+                [
+                    [1, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 2],
+                ]
+            )
+        )
+
+        assert s.output.call_count == 3
+
+        # event 1
+        e = s.output.call_args_list[0].args[0]
+        assert isinstance(e, Event)
+        assert isinstance(e.data, SingleFeature)
+        check_num_src_edges(e.data, 2)
+        check_type(e.data, 1)
+        check_points(e.data, {(0, 0)})
+
+        # event 2
+        e = s.output.call_args_list[1].args[0]
+        assert isinstance(e, Event)
+        assert isinstance(e.data, SingleFeature)
+        check_num_src_edges(e.data, 2)
+        check_type(e.data, 2)
+        check_points(e.data, {(4, 4)})
+
+        # event 3
+        e = s.output.call_args_list[2].args[0]
         assert isinstance(e, Event)
         assert isinstance(e.data, Settled)
 
@@ -120,3 +164,446 @@ class TestSingle:
         e = s.output.call_args_list[8].args[0]
         assert isinstance(e, Event)
         assert isinstance(e.data, Settled)
+
+    def test_unique(self) -> None:
+        d = VisionData(
+            [
+                [1, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert is_unique_from_neighbors(d, Point(0, 0, 1))
+
+        d = VisionData(
+            [
+                [0, 1, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert is_unique_from_neighbors(d, Point(1, 0, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 1],
+                [0, 0, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert is_unique_from_neighbors(d, Point(2, 0, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [1, 0, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert is_unique_from_neighbors(d, Point(0, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 1, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert is_unique_from_neighbors(d, Point(1, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 0, 1],
+                [0, 0, 0],
+            ]
+        )
+        assert is_unique_from_neighbors(d, Point(2, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [1, 0, 0],
+            ]
+        )
+        assert is_unique_from_neighbors(d, Point(0, 2, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 1, 0],
+            ]
+        )
+        assert is_unique_from_neighbors(d, Point(1, 2, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 1],
+            ]
+        )
+        assert is_unique_from_neighbors(d, Point(2, 2, 1))
+
+    def test_not_unique(self) -> None:
+        d = VisionData(
+            [
+                [1, 1, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(0, 0, 1))
+
+        d = VisionData(
+            [
+                [1, 0, 0],
+                [1, 0, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(0, 0, 1))
+
+        d = VisionData(
+            [
+                [1, 0, 0],
+                [0, 1, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(0, 0, 1))
+
+        d = VisionData(
+            [
+                [1, 1, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 0, 1))
+
+        d = VisionData(
+            [
+                [0, 1, 1],
+                [0, 0, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 0, 1))
+
+        d = VisionData(
+            [
+                [0, 1, 0],
+                [1, 0, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 0, 1))
+
+        d = VisionData(
+            [
+                [0, 1, 0],
+                [0, 1, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 0, 1))
+
+        d = VisionData(
+            [
+                [0, 1, 0],
+                [0, 0, 1],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 0, 1))
+
+        d = VisionData(
+            [
+                [0, 1, 1],
+                [0, 0, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(2, 0, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 1],
+                [0, 1, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(2, 0, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 1],
+                [0, 0, 1],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(2, 0, 1))
+
+        d = VisionData(
+            [
+                [1, 0, 0],
+                [1, 0, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(0, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 1, 0],
+                [1, 0, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(0, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [1, 1, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(0, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [1, 0, 0],
+                [1, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(0, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [1, 0, 0],
+                [0, 1, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(0, 1, 1))
+
+        d = VisionData(
+            [
+                [1, 0, 0],
+                [0, 1, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 1, 0],
+                [0, 1, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 1],
+                [0, 1, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [1, 1, 0],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 1, 1],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 1, 0],
+                [1, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 1, 0],
+                [0, 1, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 1, 0],
+                [0, 0, 1],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 1, 0],
+                [0, 0, 1],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(2, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 1],
+                [0, 0, 1],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(2, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 1, 1],
+                [0, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(2, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 0, 1],
+                [0, 1, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(2, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 0, 1],
+                [0, 0, 1],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(2, 1, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [1, 0, 0],
+                [1, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(0, 2, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 1, 0],
+                [1, 0, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(0, 2, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [1, 1, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(0, 2, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [1, 0, 0],
+                [0, 1, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 2, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 1, 0],
+                [0, 1, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 2, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 0, 1],
+                [0, 1, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 2, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [1, 1, 0],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 2, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 1, 1],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(1, 2, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 1, 0],
+                [0, 0, 1],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(2, 2, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 0, 1],
+                [0, 0, 1],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(2, 2, 1))
+
+        d = VisionData(
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 1, 1],
+            ]
+        )
+        assert not is_unique_from_neighbors(d, Point(2, 2, 1))
