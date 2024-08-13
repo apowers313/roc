1b88123ac796590115952936006dc6becaff9c13
Adam Powers <apowers@ato.ms>
2024-08-12 20:26:01 -0700

    add distance feature

---
+++ roc/feature_extractors/distance.py
@@ -0,0 +1,67 @@
+"""Calculates distance between Singles. Note: this is probably post-attention in
+humans, only calculated for a subset of features, and based on the saccades of the eyes."""
+
+from __future__ import annotations
+
+from ..component import Component, register_component
+from ..perception import Feature, FeatureExtractor, PerceptionEvent, Settled
+from .single import SingleFeature
+
+
+class DistanceFeature(Feature):
+    """The distance between two features"""
+
+    def __init__(
+        self, origin: Component, start: tuple[int, int], end: tuple[int, int], sz: int
+    ) -> None:
+        super().__init__(origin, "Distance")
+        self.add_size(sz)
+        self.add_point(start[0], start[1])
+        self.add_point(end[0], end[1])
+
+    def __hash__(self) -> int:
+        raise NotImplementedError("DistanceFeature hash not implemented")
+
+
+@register_component("distance", "perception")
+class Distance(FeatureExtractor[DistanceFeature]):
+    def __init__(self) -> None:
+        super().__init__()
+        self.prev_features: list[SingleFeature] = []
+
+    def event_filter(self, e: PerceptionEvent) -> bool:
+        # only listen to single:perception events
+        if e.src.name == "single" and e.src.type == "perception":
+            return True
+        return False
+
+    def get_feature(self, e: PerceptionEvent) -> Feature | None:
+        data = e.data
+
+        if isinstance(data, Settled):
+            self.prev_features.clear()
+            self.settled()
+            return None
+
+        # calculate distance between all previous elements and emit event for each
+        assert isinstance(data, SingleFeature)
+        curr_pt = data.get_point()
+        for f in self.prev_features:
+            prev_pt = f.get_point()
+            dist = chebyshev_distance(curr_pt, prev_pt)
+            self.pb_conn.send(DistanceFeature(self, curr_pt, prev_pt, dist))
+
+        # add to previous elements
+        self.prev_features.append(data)
+
+        return None
+
+
+def chebyshev_distance(p1: tuple[int, int], p2: tuple[int, int]) -> int:
+    x1 = p1[0]
+    x2 = p2[0]
+    y1 = p1[1]
+    y2 = p2[1]
+    dx = abs(x1 - x2)
+    dy = abs(y1 - y2)
+    return max(dx, dy)
---
+++ tests/distance_test.py
@@ -0,0 +1,188 @@
+# mypy: disable-error-code="no-untyped-def"
+
+from helpers.nethack_screens import screens
+from helpers.util import StubComponent, check_num_src_edges, check_points, check_size
+
+from roc.component import Component
+from roc.event import Event
+from roc.feature_extractors.distance import Distance, DistanceFeature
+from roc.feature_extractors.single import Single
+from roc.perception import Settled, VisionData
+
+
+class TestDistance:
+    def test_distance_exists(self) -> None:
+        Distance()
+
+    def test_basic(self, empty_components) -> None:
+        c = Component.get("distance", "perception")
+        assert isinstance(c, Distance)
+        s = Component.get("single", "perception")
+        assert isinstance(s, Single)
+        s = StubComponent(
+            input_bus=c.pb_conn.attached_bus,
+            output_bus=c.pb_conn.attached_bus,
+            filter=lambda e: e.src.name == "distance",
+        )
+
+        s.input_conn.send(
+            VisionData(
+                [
+                    [1, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 2],
+                ]
+            )
+        )
+
+        assert s.output.call_count == 2
+
+        # event 1
+        e = s.output.call_args_list[0].args[0]
+        assert isinstance(e, Event)
+        assert isinstance(e.data, DistanceFeature)
+        check_num_src_edges(e.data, 3)
+        check_size(e.data, 4)
+        check_points(e.data, {(0, 0), (4, 4)})
+
+        # event 2
+        e = s.output.call_args_list[1].args[0]
+        assert isinstance(e, Event)
+        assert isinstance(e.data, Settled)
+
+    def test_multi(self, empty_components) -> None:
+        c = Component.get("distance", "perception")
+        assert isinstance(c, Distance)
+        s = Component.get("single", "perception")
+        assert isinstance(s, Single)
+        s = StubComponent(
+            input_bus=c.pb_conn.attached_bus,
+            output_bus=c.pb_conn.attached_bus,
+            filter=lambda e: e.src.name == "distance",
+        )
+
+        s.input_conn.send(
+            VisionData(
+                [
+                    [1, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 2, 0],
+                    [0, 3, 0, 0, 0],
+                ]
+            )
+        )
+
+        assert s.output.call_count == 4
+
+        # event 1
+        e = s.output.call_args_list[0].args[0]
+        assert isinstance(e, Event)
+        assert isinstance(e.data, DistanceFeature)
+        check_num_src_edges(e.data, 3)
+        check_size(e.data, 3)
+        check_points(e.data, {(0, 0), (3, 3)})
+
+        # event 2
+        e = s.output.call_args_list[1].args[0]
+        assert isinstance(e, Event)
+        assert isinstance(e.data, DistanceFeature)
+        check_num_src_edges(e.data, 3)
+        check_size(e.data, 4)
+        check_points(e.data, {(0, 0), (1, 4)})
+
+        # event 3
+        e = s.output.call_args_list[2].args[0]
+        assert isinstance(e, Event)
+        assert isinstance(e.data, DistanceFeature)
+        check_num_src_edges(e.data, 3)
+        check_size(e.data, 2)
+        check_points(e.data, {(3, 3), (1, 4)})
+
+        # event 4
+        e = s.output.call_args_list[3].args[0]
+        assert isinstance(e, Event)
+        assert isinstance(e.data, Settled)
+
+    def test_screen0(self, empty_components) -> None:
+        c = Component.get("distance", "perception")
+        assert isinstance(c, Distance)
+        s = Component.get("single", "perception")
+        assert isinstance(s, Single)
+        s = StubComponent(
+            input_bus=c.pb_conn.attached_bus,
+            output_bus=c.pb_conn.attached_bus,
+            filter=lambda e: e.src.name == "distance",
+        )
+
+        s.input_conn.send(VisionData(screens[0]["chars"]))
+
+        assert s.output.call_count == 29
+
+        ##### single point (15, 4): 124 '|'
+        ##### single point (19, 4): 124 '|'
+        # distance (19, 4) (15, 4) 4
+        ##### single point (16, 5): 91 '['
+        # distance (16, 5) (15, 4) 1
+        # distance (16, 5) (19, 4) 3
+        ##### single point (17, 5): 64 '@'
+        # distance (17, 5) (15, 4) 2
+        # distance (17, 5) (19, 4) 2
+        # distance (17, 5) (16, 5) 1
+        ##### single point (18, 5): 120 'x'
+        # distance (18, 5) (15, 4) 3
+        # distance (18, 5) (19, 4) 1
+        # distance (18, 5) (16, 5) 2
+        # distance (18, 5) (17, 5) 1
+        ##### single point (19, 5): 43 '+'
+        # distance (19, 5) (15, 4) 4
+        # distance (19, 5) (19, 4) 1
+        # distance (19, 5) (16, 5) 3
+        # distance (19, 5) (17, 5) 2
+        # distance (19, 5) (18, 5) 1
+        ##### single point (16, 6): 102 'f'
+        # distance (16, 6) (15, 4) 2
+        # distance (16, 6) (19, 4) 3
+        # distance (16, 6) (16, 5) 1
+        # distance (16, 6) (17, 5) 1
+        # distance (16, 6) (18, 5) 2
+        # distance (16, 6) (19, 5) 3
+        ##### single point (19, 8): 45 '-'
+        # distance (19, 8) (15, 4) 4
+        # distance (19, 8) (19, 4) 4
+        # distance (19, 8) (16, 5) 3
+        # distance (19, 8) (17, 5) 3
+        # distance (19, 8) (18, 5) 3
+        # distance (19, 8) (19, 5) 3
+        # distance (19, 8) (16, 6) 3
+
+        # # event 1
+        # e = s.output.call_args_list[0].args[0]
+        # assert isinstance(e, Event)
+        # assert isinstance(e.data, DistanceFeature)
+        # check_num_src_edges(e.data, 3)
+        # check_size(e.data, 3)
+        # check_points(e.data, {(0, 0), (3, 3)})
+
+        # # event 2
+        # e = s.output.call_args_list[1].args[0]
+        # assert isinstance(e, Event)
+        # assert isinstance(e.data, DistanceFeature)
+        # check_num_src_edges(e.data, 3)
+        # check_size(e.data, 4)
+        # check_points(e.data, {(0, 0), (1, 4)})
+
+        # # event 3
+        # e = s.output.call_args_list[2].args[0]
+        # assert isinstance(e, Event)
+        # assert isinstance(e.data, DistanceFeature)
+        # check_num_src_edges(e.data, 3)
+        # check_size(e.data, 2)
+        # check_points(e.data, {(3, 3), (1, 4)})
+
+        # event 4
+        e = s.output.call_args_list[28].args[0]
+        assert isinstance(e, Event)
+        assert isinstance(e.data, Settled)
