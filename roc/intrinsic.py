8e48fdaef8465b4be222738e75482d7e1d6f5c58
Adam Powers <apowers@ato.ms>
2024-10-27 09:44:43 -0700

    add first pass of intrinsics

--- roc/intrinsic.py
+++ roc/intrinsic.py
@@ -1,42 +1,75 @@
-from typing import Callable
-
-from pydantic import BaseModel
+from abc import ABC, abstractmethod
+from typing import Any, Generic, TypeVar
 
 from .component import Component, register_component
-from .event import Event
+from .event import Event, EventBus
+
+# intrinsic_op_list
+
+IntrinsicType = TypeVar("IntrinsicType")
 
 
-class IntrinsicData(BaseModel):
-    pass
-
-
-IntrinsicEvent = Event[IntrinsicData]
-
-
-@register_component("intrinsic", "intrinsic")
-class Intrinsic(Component):
-    pass
-
-
-IntrinsicFn = Callable[[IntrinsicEvent], None]
-intrinsic_registry: dict[str, IntrinsicFn] = {}
-
-
-class register_intrinsic:
-    """Decorator for registering intrinsics."""
-
+class IntrinsicOp(ABC, Generic[IntrinsicType]):
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def __call__(self, fn: IntrinsicFn) -> IntrinsicFn:
-        if self.name in intrinsic_registry:
-            raise ValueError(f"Registering duplicate intrinsic '{self.name}'")
+    @abstractmethod
+    def validate(self, val: IntrinsicType) -> bool: ...
 
-        intrinsic_registry[self.name] = fn
+    @abstractmethod
+    def normalize(self, val: IntrinsicType) -> float: ...
 
-        return fn
+    # register
+
+    # deregister
+
+    # clear
 
 
-@register_intrinsic("no-op")
-def noop_intrinsic(e: IntrinsicEvent) -> None:
-    pass
+class IntrinsicIntOp(IntrinsicOp[int]):
+    def __init__(self, name: str, min: int, max: int) -> None:
+        super().__init__(name)
+        self.min = min
+        self.max = max
+        self.range = abs(min) + abs(max)
+
+    def validate(self, val: int) -> bool:
+        if (val < self.min) or (val > self.max):
+            return False
+
+        return True
+
+    def normalize(self, val: int) -> float:
+        return (val + abs(self.min)) / self.range
+
+
+class IntrinsicBoolOp(IntrinsicOp[bool]):
+    def validate(self, val: bool) -> bool:
+        return True
+
+    def normalize(self, val: bool) -> float:
+        if val:
+            return 1.0
+
+        return 0.0
+
+
+IntrinsicData = dict[str, Any]
+IntrinsicEvent = Event[IntrinsicData]
+
+
+@register_component("intrinsic", "intrinsic", auto=True)
+class Intrinsic(Component):
+    bus = EventBus[IntrinsicData]("intrinsic")
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.int_conn = self.connect_bus(Intrinsic.bus)
+        self.int_conn.listen(self.do_intrinsic)
+
+    def event_filter(self, e: IntrinsicEvent) -> bool:
+        print("Intrinsic event filter")
+        return True
+
+    def do_intrinsic(self, e: IntrinsicEvent) -> None:
+        return None
--- tests/intrinsic_test.py
+++ tests/intrinsic_test.py
@@ -1,8 +1,53 @@
 # mypy: disable-error-code="no-untyped-def"
 
-from roc.intrinsic import intrinsic_registry
+from helpers.nethack_blstats import blstat0 as test_blstat
+from helpers.util import StubComponent
+
+from roc.component import Component
+from roc.intrinsic import Intrinsic, IntrinsicBoolOp, IntrinsicIntOp
 
 
-class TestIntrinsicRegistry:
-    def test_registered(self):
-        assert "no-op" in intrinsic_registry
+class TestIntrinsicInt:
+    def test_validate(self) -> None:
+        op = IntrinsicIntOp("test", -2, 8)
+        assert not op.validate(-3)
+        assert op.validate(-2)
+        assert op.validate(3)
+        assert op.validate(8)
+        assert not op.validate(9)
+
+    def test_normalize(self) -> None:
+        op = IntrinsicIntOp("test", -2, 8)
+        assert op.range == 10
+        assert op.normalize(-2) == 0
+        assert op.normalize(3) == 0.5
+        assert op.normalize(8) == 1
+
+
+class TestIntrinsicBool:
+    def test_validate(self) -> None:
+        op = IntrinsicBoolOp("test")
+        assert op.validate(True)
+        assert op.validate(False)
+
+    def test_normalize(self) -> None:
+        op = IntrinsicBoolOp("test")
+        assert op.normalize(False) == 0
+        assert op.normalize(True) == 1
+
+
+class TestIntrinsic:
+    def test_exists(self) -> None:
+        Intrinsic()
+
+    def test_basic(self) -> None:
+        intrinsic = Component.get("intrinsic", "intrinsic")
+        assert isinstance(intrinsic, Intrinsic)
+        s = StubComponent(
+            input_bus=intrinsic.int_conn.attached_bus,
+            output_bus=intrinsic.int_conn.attached_bus,
+        )
+
+        s.input_conn.send(test_blstat)
+
+        # assert s.output.call_count == 3
