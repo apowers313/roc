b3091e2dfd32f31d392d4f1423783464f2d23a4c
Adam Powers <apowers@ato.ms>
2025-03-16 09:36:55 -0700

    refactor sequencer events

--- roc/sequencer.py
+++ roc/sequencer.py
@@ -4,7 +4,7 @@ from pydantic import Field
 
 from roc.action import Action, TakeAction
 from roc.component import Component, register_component
-from roc.event import Event
+from roc.event import Event, EventBus
 from roc.graphdb import Edge, EdgeConnectionsList, Node
 from roc.intrinsic import Intrinsic, IntrinsicData
 from roc.object import Object, ObjectResolver
@@ -28,6 +28,7 @@ class FrameAttributes(Edge):
     allowed_connections: EdgeConnectionsList = [
         ("Frame", "Object"),
         ("Frame", "TakeAction"),
+        ("TakeAction", "Frame"),
         ("Frame", "IntrinsicNode"),
     ]
 
@@ -38,14 +39,17 @@ class NextFrame(Edge):
 
 @register_component("sequencer", "sequencer", auto=True)
 class Sequencer(Component):
+    bus = EventBus[Frame]("sequencer")
+
     def __init__(self) -> None:
         super().__init__()
+        self.sequencer_conn = self.connect_bus(Sequencer.bus)
         self.obj_res_conn = self.connect_bus(ObjectResolver.bus)
-        self.obj_res_conn.listen(self.event_collector)
+        self.obj_res_conn.listen(self.handle_object_resolution_event)
         self.action_conn = self.connect_bus(Action.bus)
-        self.action_conn.listen(self.event_collector)
+        self.action_conn.listen(self.handle_action_event)
         self.intrinsic_conn = self.connect_bus(Intrinsic.bus)
-        self.intrinsic_conn.listen(self.event_collector)
+        self.intrinsic_conn.listen(self.handle_intrinsic_event)
         self.last_frame: Frame | None = None
         self.current_frame: Frame = Frame()
 
@@ -56,17 +60,26 @@ class Sequencer(Component):
             or isinstance(e.data, IntrinsicData)
         )
 
-    def event_collector(self, e: Event[Any]) -> None:
+    def handle_object_resolution_event(self, e: Event[Any]) -> None:
+        FrameAttributes.connect(self.current_frame, e.data)
+
+    def handle_intrinsic_event(self, e: Event[Any]) -> None:
+        for intrinsic_node in e.data.to_nodes():
+            FrameAttributes.connect(self.current_frame, intrinsic_node)
+
+    def handle_action_event(self, e: Event[Any]) -> None:
         # start new frame on action
         if isinstance(e.data, TakeAction):
-            if self.last_frame is not None:
-                NextFrame.connect(self.last_frame, self.current_frame)
+            # connect action data to the old frame
+            FrameAttributes.connect(self.current_frame, e.data)
 
+            # create a new frame and connect the previous frame to the current frame
             self.last_frame = self.current_frame
             self.current_frame = Frame()
-            FrameAttributes.connect(self.current_frame, e.data)
-        elif isinstance(e.data, Object):
-            FrameAttributes.connect(self.current_frame, e.data)
-        elif isinstance(e.data, IntrinsicData):
-            for intrinsic_node in e.data.to_nodes():
-                FrameAttributes.connect(self.current_frame, intrinsic_node)
+            NextFrame.connect(self.last_frame, self.current_frame)
+
+            # connect action data to the new frame
+            FrameAttributes.connect(e.data, self.current_frame)
+
+            # emit the frame on the bus
+            self.sequencer_conn.send(self.last_frame)
--- tests/sequencer_test.py
+++ tests/sequencer_test.py
@@ -1,6 +1,94 @@
-from roc.sequencer import Sequencer
+# mypy: disable-error-code="no-untyped-def"
+
+from helpers.util import StubComponent
+
+from roc.action import Action, TakeAction
+from roc.component import Component
+from roc.event import Event
+from roc.intrinsic import Intrinsic, IntrinsicData, IntrinsicNode
+from roc.object import Object, ObjectResolver
+from roc.sequencer import Frame, Sequencer
 
 
 class TestSequencer:
-    def test_exists(self) -> None:
+    def test_exists(self, empty_components) -> None:
         Sequencer()
+
+    def test_action(self, empty_components) -> None:
+        sequencer = Component.get("sequencer", "sequencer")
+        assert isinstance(sequencer, Sequencer)
+        action = Component.get("action", "action")
+        assert isinstance(action, Action)
+
+        s = StubComponent(
+            input_bus=action.action_bus_conn.attached_bus,
+            output_bus=sequencer.sequencer_conn.attached_bus,
+        )
+
+        take_action = TakeAction(action=20)
+        s.input_conn.send(take_action)
+        assert s.output.call_count == 1
+
+        # first event
+        e = s.output.call_args_list[0].args[0]
+        assert isinstance(e, Event)
+        assert isinstance(e.data, Frame)
+        frame = e.data
+        assert len(frame.edges) == 2
+        assert len(frame.src_edges) == 2
+        assert frame.src_edges[0].type == "FrameAttributes"
+        assert frame.src_edges[0].dst is take_action
+        assert frame.src_edges[1].type == "NextFrame"
+        assert isinstance(frame.src_edges[1].dst, Frame)
+
+    def test_basic(self, empty_components) -> None:
+        sequencer = Component.get("sequencer", "sequencer")
+        assert isinstance(sequencer, Sequencer)
+        action = Component.get("action", "action")
+        assert isinstance(action, Action)
+        object_resolver = Component.get("resolver", "object")
+        assert isinstance(object_resolver, ObjectResolver)
+        intrinsic = Component.get("intrinsic", "intrinsic")
+        assert isinstance(intrinsic, Intrinsic)
+
+        s = StubComponent(
+            input_bus=action.action_bus_conn.attached_bus,
+            output_bus=sequencer.sequencer_conn.attached_bus,
+        )
+
+        o = Object()
+        object_resolver.obj_res_conn.send(o)
+        assert s.output.call_count == 0
+
+        intrinsic.int_conn.send(IntrinsicData({"hunger": 1, "hp": 7, "hpmax": 14}))
+        assert s.output.call_count == 0
+
+        a = TakeAction(action=20)
+        s.input_conn.send(a)
+        assert s.output.call_count == 1
+
+        # first event
+        e = s.output.call_args_list[0].args[0]
+        assert isinstance(e, Event)
+        assert isinstance(e.data, Frame)
+        frame = e.data
+        print(frame.src_edges[3].dst)
+        assert len(frame.edges) == 5
+        assert len(frame.src_edges) == 5
+        # object
+        assert frame.src_edges[0].type == "FrameAttributes"
+        assert frame.src_edges[0].dst is o
+        # hp intrinsic
+        assert frame.src_edges[1].type == "FrameAttributes"
+        assert isinstance(frame.src_edges[1].dst, IntrinsicNode)
+        assert frame.src_edges[1].dst.name in {"hunger", "hp"}
+        # hunger intrinsic
+        assert frame.src_edges[2].type == "FrameAttributes"
+        assert isinstance(frame.src_edges[2].dst, IntrinsicNode)
+        assert frame.src_edges[2].dst.name in {"hunger", "hp"}
+        # action
+        assert frame.src_edges[3].type == "FrameAttributes"
+        assert frame.src_edges[3].dst is a
+        # next frame
+        assert frame.src_edges[4].type == "NextFrame"
+        assert isinstance(frame.src_edges[4].dst, Frame)
